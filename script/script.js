2f576196870dcdbc976f0eb84482efc696bdc7b5
Julian <yuffie069@gmail.com>
2023-05-29 22:35:51 -0300

    agregamos el script de la pagina

---
+++ script/script.js
@@ -0,0 +1,77 @@
+const textArea = document.querySelector(".Escrito");
+const mensaje = document.querySelector(".texto");
+const copia = document.querySelector(".botonCopiar");
+copia.style.display = "none"
+
+
+function validarTexto(){
+     let textoEscrito = document.querySelector(".Escrito").value;
+    let validador = textoEscrito.match(/^[a-z ]*$/);
+
+    if(!validador || validador === 0) {
+        alert("Recuerda que Solo son permitidas letras minúsculas y sin acentos o caracteres especiales")
+        location.reload();
+        return true;    }
+}
+
+
+function btnEncriptar(){
+ if(!validarTexto()) {
+        const textoEncriptado = encriptar(textArea.value)
+        mensaje.value = textoEncriptado
+        mensaje.style.backgroundImage = "none"
+        textArea.value = "";
+        copia.style.display = "block"
+    
+    }
+}
+
+
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+        }
+
+    }
+    return stringEncriptada
+}
+
+
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    
+}
+
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1] , matrizCodigo[i][0])
+
+        }
+
+    }
+    return stringDesencriptada
+}
+
+
+function copiar(){
+    mensaje.select();
+    navigator.clipboard.writeText(mensaje.value)
+    mensaje.value = "";
+    
+}
+
+
